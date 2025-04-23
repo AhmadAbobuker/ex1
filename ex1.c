87dcb418e2935fd24faf764e584f25a403277914
ahmad <ahmadabobuker15@outlook.com>
2025-04-23 21:14:01 +0300

    Ex1 is ready for submition all the outputs are the same change 2

--- ex1.c
+++ ex1.c
@@ -19,7 +19,7 @@ int main() {
   // If its 1 at that postition and we do the logic gate "and" it should return 1 since its true and vice versa .
   // the number that we wanna use in such a case is a number that has on that poistion 1 in its binary form and nothing else .
   unsigned int TheOne = (1 << Position);
-  printf("The bit in position %d of number 45 is :%d\n" ,Position,((Number & TheOne))>>Position);// A complication is that computers translate machine language to human form  .
+  printf("The bit in position %d of number %d is :%d\n" ,Position,Number,((Number & TheOne))>>Position);// A complication is that computers translate machine language to human form  .
 
 
 
@@ -30,8 +30,8 @@ int main() {
   printf("Please enter a position:\n");
   scanf("%d", &Position);
   TheOne = (1 << Position);
-  printf("Number with bit %d set to 1 :%d\n", Position, (Number | TheOne));//using the logic gate OR so if its 1 it wont change the answer and if its 0 it changes to 1
-  printf("Number with bit %d set to 0 :%d\n", Position, ((Number | TheOne)^TheOne));// by using XOR on the OR form , we make sure that if its changed it reverts , and if not it will now .
+  printf("Number with bit %d set to 1 : %d\n", Position, (Number | TheOne));//using the logic gate OR so if its 1 it wont change the answer and if its 0 it changes to 1
+  printf("Number with bit %d set to 0 : %d\n", Position, ((Number | TheOne)^TheOne));// by using XOR on the OR form , we make sure that if its changed it reverts , and if not it will now .
 
 
 
@@ -41,7 +41,7 @@ int main() {
   printf("Please enter a position:\n");
   scanf("%d", &Position);
   TheOne = (1 << Position);
-  printf("Number with bit %d toggled :%d\n", Position, (Number  ^ TheOne));//Since TheOne is awlays 1 ,XOR gate fits ,its either 0 or 1 = 1 | 1 or 1 = 0 .
+  printf("Number with bit %d toggled: %d\n", Position, (Number  ^ TheOne));//Since TheOne is awlays 1 ,XOR gate fits ,its either 0 or 1 = 1 | 1 or 1 = 0 .
 
 
 
@@ -58,9 +58,9 @@ int main() {
 
       int Number1, Number2;
     printf("\n3, 5, 7, 11:\n");
-    printf("Please enter the first number(octal):\n");
+    printf("Please enter the first number (octal):\n");
     scanf("%d", &Number1);
-    printf("Please enter the seconde number(octal):\n");
+    printf("Please enter the seconde number (octal):\n");
     scanf("%d", &Number2);
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -144,7 +144,7 @@ int main() {
     HD7 = ((Number1 + Number2) / square16) % 16;      square16 = square16 * 16;
 
     HD8 = ((Number1 + Number2) / square16) % 16;
-    printf("The sum in hexadecimal:%X\n" , HD1*1+HD2*16+HD3*16*16+HD4*16*16*16+HD5*16*16*16*16+HD6*16*16*16*16*16+HD7*16*16*16*16*16*16+HD8*16*16*16*16*16*16*16 );
+    printf("The sum in hexadecimal: %X\n" , HD1*1+HD2*16+HD3*16*16+HD4*16*16*16+HD5*16*16*16*16+HD6*16*16*16*16*16+HD7*16*16*16*16*16*16+HD8*16*16*16*16*16*16*16 );
 
    // printf("%X %X %X %X %X %X %X %X\n", HD8, HD7, HD6, HD5, HD4, HD3, HD2, HD1);
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -155,7 +155,7 @@ int main() {
     bit5 = (HD2 & (1 << 1)) >> 1;
     bit7 = (HD2 & (1 << 3)) >> 3;
     bit11 = (HD3 & (1 << 3)) >>3;
-    printf("The 3,5,7,11 bits are:%d%d%d%d\n", bit3, bit5, bit7, bit11);
+    printf("The 3,5,7,11 bits are: %d%d%d%d\n", bit3, bit5, bit7, bit11);
 
   printf("Bye!\n");
   
